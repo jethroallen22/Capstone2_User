97c5772ff8a67bb5ce09f738ce5eeebc74e3ca93
Marius Verdan <mariusverdan99@gmail.com>
2023-06-15 07:54:18 +0800

    Voucher Complete

---
+++ app/src/main/java/com/example/myapplication/activities/models/IPModel.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.activities.models;
+
+public class IPModel {
+    private String URL = "http://10.209.87.21/mosibus_php/user/";
+
+    public IPModel() {
+    }
+
+    public String getURL() {
+        return URL;
+    }
+}
